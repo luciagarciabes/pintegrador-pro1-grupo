fe3a37e78a3947c764c35ece98b17b62d3a9df57
luciagarciabes <lgarciabes@udesa.edu.ar>
2022-11-04 18:52:36 -0300

    fetch

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-
-
+let urlSeries = "https://api.themoviedb.org/3/tv/{tv_id}?api_key=a999f9c45003fc79555aea4968543ddf&language=en-US"
+let urlPeliculas = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=a999f9c45003fc79555aea4968543dd&language=en-US"
 
 fetch()
 .then(function (respuesta) {
-    return respuesta
+    return respuesta.json
 })
 .then(function (data) {
     return data
