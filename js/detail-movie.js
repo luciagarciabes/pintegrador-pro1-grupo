7420b12651336abeb1c017009072f02eb4812bf7
luciagarciabes <lgarciabes@udesa.edu.ar>
2022-11-09 21:01:37 -0300

    avanzo con el index y el detalle de peliculas

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -5,10 +5,13 @@ let idPelicula= qsObj.get('idPelicula')
 
 /* api key y el endpoint de detalle de peliculas*/
 let api_key = "a999f9c45003fc79555aea4968543ddf";
-let peliculaDetalle= `https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${api_key}language=en-US`
+let peliculaDetalle= `https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${api_key}&language=en-US`
+
 
 /* Capturo elementos*/
 let img= document.querySelector(".img-detalle-pelicula");
+let textoDetalleMmovie= document.querySelector(".texto-detalle-movie");
+let nombreDetallePelicula= document.querySelector(".nombre-detalle-pelicula")
 
 fetch(peliculaDetalle)
 .then(function (respuesta) {
@@ -16,6 +19,23 @@ fetch(peliculaDetalle)
 })
 .then(function (data) {
     console.log(data)
+    let pelicula= data
+    let contenido= `<ul>
+                        <article class="flex">
+                            <li><strong>${pelicula.release_date}</strong></li>
+                            <li><strong>${pelicula.runtime} minutos </strong></li>
+                            <li><strong>${pelicula.genres}</strong></li> .   
+                        </article>
+
+                        <li class="informacion">${pelicula.overview}</li>
+                        <li class="informacion"><strong>Dirección:</strong> ${pelicula.production_companies}</li>
+                        <li class="lifav"><a class="botonfav" href="./favorite.html"> Agregar a favoritos </a></li>
+
+                    </ul>`
+                    /* PROBLEMAS CON LOS GENEROS Y DIRECTORES */
+    nombreDetallePelicula.innerText= pelicula.original_title
+    textoDetalleMmovie.innerHTML= contenido;
+    img.src=`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`
     return data
 })
 .catch(function (error) {
@@ -23,6 +43,6 @@ fetch(peliculaDetalle)
 })
 
 
-/*NO ME ESTA TOMANDO LA API KEY< ME DICE ERROR. */
+
 
 
--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 let api_key = "a999f9c45003fc79555aea4968543ddf";
 let peliculasPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
 let seriesPopulares= `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=en-US&page=1`
-let loMasVisto= `https://api.themoviedb.org/3/trending/all/day?api_key=${api_key}`
+let loMasVisto= `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=en-US&page=1`
 
 /* capturamos los elementos*/ 
 let lista_principal_peliculas= document.querySelector(".lista_principal_peliculas");
@@ -22,7 +22,7 @@ fetch(peliculasPopulares)
     let contenido1= ""
     for (let i = 0; i < 5; i++) {
         contenido1+= `<li class="cada_titulo">
-                        <a href="./detail-movie.html"> 
+                        <a href="./detail-movie.html?idPelicula=${peliculas[i].id}"> 
                         <img class="imagenes_home" src="https://image.tmdb.org/t/p/w500${peliculas[i].poster_path}" alt="${peliculas[i].title}"
                             height="250px">
                         <ul class="lista_anidada">
@@ -54,7 +54,7 @@ fetch(seriesPopulares)
     let contenido2= ""
     for (let i = 0; i < 5; i++) {
       contenido2+= `<li class="cada_titulo">
-                        <a href="./detail-serie.html"><img class="imagenes_home" src="" alt=" ${series[i].name}"   
+                        <a href="./detail-serie.html?idSerie=${series[i].id}"><img class="imagenes_home" src="https://image.tmdb.org/t/p/w300${series[i].poster_path}" alt=" ${series[i].name}"   
                             height="250px">
                             <ul class="lista_anidada">
                             <li class="li_piedefoto"> ${series[i].name} </li>
@@ -82,16 +82,16 @@ fetch(loMasVisto)
 })
 .then(function (data) {
     console.log(data.results);
-    trending= data.results;
+    topRated= data.results;
     contenido3= ""
 
     for (let i = 0; i < 5; i++) {
        contenido3+=  `<li class="cada_titulo">
-                            <a href="./detail-serie.html"> <img class="imagenes_home" src="./img/friends.webp" alt="${trending[i].original_title}"
+                            <a href="./detail-serie.html?idMasVisto=${topRated[i].id}"> <img class="imagenes_home" src="https://image.tmdb.org/t/p/w300${topRated[i].poster_path}" alt="${topRated[i].original_title}"
                                 height="250px">
                                 <ul class="lista_anidada">
-                                <li class="li_piedefoto"> ${trending[i].original_title}</li>
-                                <li class="li_piedefoto">Estreno:  ${trending[i].release_date} </li>
+                                <li class="li_piedefoto"> ${topRated[i].original_title}</li>
+                                <li class="li_piedefoto">Estreno:  ${topRated[i].release_date} </li>
                                 <li class="vermas"> Ver más </li>
                                 </ul>
                             </a>
@@ -105,4 +105,6 @@ fetch(loMasVisto)
     return error
 })
 
-/*  De donde sacamos los titulos para peliculas y series mas vistos ==> es trending?*/
+/*  De donde sacamos los titulos para peliculas y series mas vistos ==> es trending?      conviene? cuando queremos poner el id deberia hacer una diferencia entre las paginas de detalle serie y pelicula? serie mejor poner solo peliculas o solo series?
+
+ME PARECE que vamos a tener que elegir solo un tipo porque sino los nombres de las propiedades cambian y por eso se muestra el titulo en algunas si y en otras no*/
