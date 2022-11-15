71eb1aa856048409b7316059f96d5a06c374ab4f
martinascarfoni <mscarfoni@udesa.edu.ar>
2022-11-15 17:05:21 -0300

    el fetch de proveedores

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -6,7 +6,7 @@ let idPelicula = qsObj.get('idPelicula')
 /* api key y el endpoint de detalle de peliculas*/
 let api_key = "a999f9c45003fc79555aea4968543ddf";
 let peliculaDetalle = `https://api.themoviedb.org/3/movie/${idPelicula}?api_key=${api_key}&language=en-US`
-let urlProveedores = `https://api.themoviedb.org/3/watch/providers/movie?api_key=${api_key}&language=en-US`
+let urlProveedores = `https://api.themoviedb.org/3/movie/${idPelicula}/watch/providers?api_key=${api_key}`
 
 
 /* Capturo elementos*/
@@ -29,32 +29,6 @@ fetch(peliculaDetalle)
         for (let i = 0; i < generos.length; i++) {
             infoGeneros += ' ' + generos[i]['name'] + ',  '
         }
-
-
-        /* Fetch para los proveedores*/
-        fetch(urlProveedores)
-            .then(function (respuesta) {
-                return respuesta.json()
-
-            })
-            .then(function (data2) {
-                console.log(data2);
-                let arrayProveedores = data.results;
-                 let contenidoProveedores=""
-                for (let i = 0; i < 3; i++) {
-                     contenidoProveedores += `<li> 
-                                                    <h3> ${arrayProveedores[i].provider_name}</h3>
-                                                    <img src="https://image.tmdb.org/t/p/w500${arrayProveedores.logo_path}" alt="${arrayProveedores[i.provider_name]}">
-                                                </li>`
-                }
-                listaPlataformas.innerHTML= contenidoProveedores
-            
-
-            })
-            .catch(function (error) {
-                return error
-
-            })
 
 
         let contenido = `<ul>
@@ -84,7 +58,40 @@ fetch(peliculaDetalle)
     })
 
 
+/* Fetch para los proveedores*/
+fetch(urlProveedores)
+.then(function (respuesta) {
+    return respuesta.json()
 
+})
+.then(function (data2) {
+    console.log(data2.results);
+
+    if (data2.results.MX.buy.length < 0) {
+        console.log(data2.results.MX.buy);
+        let arrayProveedores = data2.results.MX.buy;
+        let contenidoProveedores = ""
+
+        for (let i = 0; i < arrayProveedores.length; i++) {
+            contenidoProveedores += `<li> 
+                                        <h3> ${arrayProveedores[i].provider_name}</h3>
+                                        <img src="https://image.tmdb.org/t/p/w500${arrayProveedores.logo_path}" alt="${arrayProveedores[i.provider_name]}">
+                                    </li>`
+                                }
+                            
+    listaPlataformas.innerHTML= contenidoProveedores
+
+    } else {
+        Selection.innerText = "no hay proveedores"; 
+    }
+
+})
+.catch(function (error) {
+    return error
+})
+
+
+/* if data.results.MX undefined o if MX != "" */
 
 
 
