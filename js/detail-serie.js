b012cc96eca695949cfceab830b3bf5896a003a6
Clari-Battaglia <clarabatta@gmail.com>
2022-11-18 10:10:10 -0300

    ver recomendaciones series

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -7,7 +7,7 @@ let idSerie = qsObj.get('idSerie');
 let api_key = "a999f9c45003fc79555aea4968543ddf";
 let serieDetalle= `https://api.themoviedb.org/3/tv/${idSerie}?api_key=${api_key}&language=en-US`;
 let urlProveedores = `https://api.themoviedb.org/3/tv/${idSerie}/watch/providers?api_key=${api_key}`
-urlRecomendaciones=`https://api.themoviedb.org/3/tv/${idSerie}/recommendations?api_key=${api_key}&language=en-US&page=1`
+ let urlRecomendaciones=`https://api.themoviedb.org/3/tv/${idSerie}/recommendations?api_key=${api_key}&language=en-US&page=1`
 
 /* Capturo elementos */
 let img = document.querySelector(".img-detalle-titulos");
@@ -44,11 +44,11 @@ fetch(serieDetalle)
         img.src = `https://image.tmdb.org/t/p/w500${serie.poster_path}`
         estreno.innerText = serie.first_air_date;
         temporada.innerText = `temporadas ${serie.number_of_seasons}`
-        rating.innerText = serie.vote_average;
+        rating.innerText = ` Rating: ${serie.vote_average}`;
         extra.innerText = serie.overview;
         geneross.innerHTML = `Géneros: ${infoGeneros}`
         botonagregarfav.innerHTML = `<a class="botonfav" href="./favorite.html?idSerie=${serie.id}"> Agregar a favoritos </a>`
-        botonrecomendacion.innerHTML = `<a class="botonfav botonrecomendacion"> Ver recomendaciones </a>`
+       
 
     })
     .catch(function (error) {
@@ -91,7 +91,7 @@ fetch(urlProveedores)
 
 
 
-
+let contenidoRecomendaciones = "";
 /* Fetch del Ver recomendaciones */
 fetch(urlRecomendaciones)
     .then(function (respuesta) {
@@ -101,26 +101,50 @@ fetch(urlRecomendaciones)
     .then(function (data) {
         console.log(data.results)
         let seriesRecomendadas = data.results;
-        let contenidoRecomendaciones = "";
+        
 
         for (let i = 0; i < 4; i++) {
             contenidoRecomendaciones += `<li class="cada_titulo">
-                                    <a href="./detail-serie.html?idSerie=${seriesRecomendadas[i].id}"><img class="imagenes_home" src="https://image.tmdb.org/t/p/w500${seriesRecomendadas[i].poster_path}" alt=" ${series[i].original_name}"   
-                                        height="250px">
-                                        <ul class="lista_anidada">
-                                        <li class="li_piedefoto"> ${seriesRecomendadas[i].original_name} </li>
-                                        <li class="li_piedefoto">Estreno: ${seriesRecomendadas[i].first_air_date} </li>
-                                        <li class="vermas"> Ver más </li>
-
-                                        </ul> </a>
-                                </li>` };
+                                            <a href="./detail-serie.html?idSerie=${seriesRecomendadas[i].id}">
+                                                <img class="imagenes_home" src="https://image.tmdb.org/t/p/w500${seriesRecomendadas[i].poster_path}" alt=" ${seriesRecomendadas[i].original_name}"   
+                                                    height="250px">
+                                                    <ul class="lista_anidada">
+                                                    <li class="li_piedefoto"> ${seriesRecomendadas[i].original_name} </li>
+                                                    <li class="li_piedefoto">Estreno: ${seriesRecomendadas[i].first_air_date} </li>
+                                                    <li class="vermas"> Ver más </li>
+                                                    </ul>
+                                            </a>
+                                        </li>`
+            
+        };
         ulverrecomendacionesSeries.innerHTML = contenidoRecomendaciones
+
+    
+      
 
     })
     .catch(function (error) {
         return error
 
     })
+
+     /* Seccion de recomendaciones*/
+let muestraRecomendaciones= false;
+
+botonrecomendacion.addEventListener('click', function(e) {
+    
+console.log("CLIC")
+    if (muestraRecomendaciones) {
+        ulverrecomendacionesSeries.style.display = 'none';
+        botonrecomendacion.innerText = 'Ver recomendaciones'
+        muestraRecomendaciones = false;
+    } else {
+        ulverrecomendacionesSeries.style.display = 'flex';
+        botonrecomendacion.innerText = 'Ocultar recomendaciones'
+        muestraRecomendaciones = true;
+    }
+})
+
 
 
 
